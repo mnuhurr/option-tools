8ae6f728910239a5ad798c3eb41cb833b0766ab8
Manu Harju <manu.harju@gmail.com>
2020-08-18 00:24:01 +0300

    comments

--- bs.hpp
+++ bs.hpp
@@ -1,15 +1,21 @@
-
+/*
+ * methods for option pricing with black-scholes-merton model. also methods for implied volatility.
+ *  
+ * */
 
 namespace black_scholes {
+    // some constants for numeric recipes
     const double iv_min_sigma = 1e-6;
     const double iv_max_sigma = 100.0;
 
     const unsigned int iv_max_rounds = 1000;
     const double iv_tolerance = 1e-9;
 
+    // option pricing
     double call_price(double S, double K, double t, double r, double sigma);
     double put_price(double S, double K, double t, double r, double sigma);
 
+    // implied volatility
     double call_impvol(double S, double K, double t, double C, double r);
     double put_impvol(double S, double K, double t, double P, double r);
 }
